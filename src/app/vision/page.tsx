d561264599a59d4cf67fce1c83c69f578d891f1d
Roberto Shimizu <rshimizubr@gmail.com>
2023-12-17 15:05:04 -0300

    pause agents with vision

--- src/app/vision/page.tsx
+++ src/app/vision/page.tsx
@@ -45,8 +45,13 @@ export default function Chat() {
 
     console.log(formData);
 
+    // It uses GPT-4 Turbo with vision
+
     handleSubmit(e, {
-      data: formData
+      data: {
+        imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Field_sparrow_in_CP_%2841484%29_%28cropped%29.jpg/733px-Field_sparrow_in_CP_%2841484%29_%28cropped%29.jpg',
+      },
     });
     
   }
