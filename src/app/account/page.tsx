64ffeb73fc3e7572bec64fc6a411d458e76f745e
Roberto Shimizu <rshimizubr@gmail.com>
2023-12-19 08:55:23 -0300

    add supabase deps and middleware

---
+++ src/app/account/page.tsx
@@ -0,0 +1,13 @@
+
+export default function UserAccount() {
+
+  return (
+    <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
+      <div className="whitespace-pre-wrap">
+        User Profile Page
+      </div>
+      
+
+    </div>
+  );
+}
