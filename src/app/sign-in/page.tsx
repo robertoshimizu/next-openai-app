fde0c016e13d493be3ad76ba13e53d25e3f6cfba
Roberto Shimizu <rshimizubr@gmail.com>
2023-12-19 10:16:32 -0300

    add and styled Sign-In UI

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -3,19 +3,49 @@ import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { Database } from '@/app/types/supabase'
+import { getURL } from '../utils/helpers'
+import Logo from '@/components/icons/Logo'
 
 export default function AuthForm() {
   const supabase = createClientComponentClient<Database>()
 
   return (
-    <Auth
-      supabaseClient={supabase}
-      view="magic_link"
-      appearance={{ theme: ThemeSupa }}
-      theme="dark"
-      showLinks={false}
-      providers={[]}
-      redirectTo="http://localhost:3000/auth/callback"
-    />
+    <div className="flex justify-center height-screen-helper">
+      <div className="flex flex-col justify-between max-w-lg p-3 m-auto w-80 ">
+        <div className="flex justify-center pb-12 ">
+          <Logo width="64px" height="64px" />
+        </div>
+        <div className="flex flex-col space-y-4">
+      <Auth
+        supabaseClient={supabase}
+        providers={['google', 'facebook', 'apple']}
+        redirectTo={`${getURL()}/auth/callback`}
+        magicLink={true}
+        appearance={{
+          theme: ThemeSupa,
+          variables: {
+            default: {
+              colors: {
+                brand: '#404040',
+                brandAccent: '#52525b'
+              }
+            }
+          }
+        }}
+        theme="dark"
+      />
+    </div>
+      </div>
+    </div>
+    
+    // <Auth
+    //   supabaseClient={supabase}
+    //   view="magic_link"
+    //   appearance={{ theme: ThemeSupa }}
+    //   theme="dark"
+    //   showLinks={false}
+    //   providers={[]}
+    //   redirectTo="http://localhost:3000/auth/callback"
+    // />
   )
 }
